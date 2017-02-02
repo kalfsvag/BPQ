7807861b0982996778dd5e5cd0eeb90c66dc312a
Kristian Alfsvag <kristian@alfsvag.no>
2017-02-02 14:49:35 +0100

    Try to prove that the map to the group completion is injective

--- group_completion.v
+++ group_completion.v
@@ -1,6 +1,6 @@
 Require Import HoTT.
 Require Import UnivalenceAxiom.
-Load stuff.
+Load monoids_and_groups. (*stuff.v should also be loaded now?*)
 
 Section Group_completion.
   (*The Grothendieck group completion*)
@@ -17,8 +17,10 @@ Section Group_completion.
     exact (a+s, b+s).
   Defined.
 
-  Definition grp_compl_set (S:Monoid) := Trunc 0 (Coeq as_bs fst).
-  Definition grp_compl_mult : grp_compl_set S -> grp_compl_set S -> grp_compl_set S.
+  Definition grp_compl_set := Trunc 0 (Coeq as_bs fst).
+  Definition grp_compl  (a b : S) : grp_compl_set
+    := tr (coeq (a, b)).
+  Definition grp_compl_mult : grp_compl_set -> grp_compl_set -> grp_compl_set.
     refine (Trunc_rec _).
     intro g1.
     refine (Trunc_rec _).
@@ -31,7 +33,7 @@ Section Group_completion.
       srapply @Coeq_rec.
       + (*Fix first variable*)
         intros [m1 m2].
-        exact (tr (coeq (m1 + n1, m2 + n2))).
+        exact (grp_compl (m1 + n1) (m2 + n2)).
       + (*First variable runs along cp*)
         intros [[a b] s].
         apply (ap (tr)).
@@ -86,7 +88,7 @@ Section Group_completion.
         intro abs. apply (istrunc_truncation 0).
     Defined.
           
-  Definition grp_compl_id : grp_compl_set S := tr (coeq (mon_id S, mon_id S)).
+  Definition grp_compl_id : grp_compl_set := grp_compl (mon_id S) (mon_id S).
   
   Definition grp_compl_lid : left_identity grp_compl_mult grp_compl_id.
   Proof.
@@ -136,12 +138,12 @@ Section Group_completion.
       intro abs. apply (istrunc_truncation 0).
   Defined.    
 
-  Definition grp_compl_inv : grp_compl_set S -> grp_compl_set S.
+  Definition grp_compl_inv : grp_compl_set -> grp_compl_set.
   Proof.
     apply Trunc_rec.
     srapply @Coeq_rec.
     - intros [m1 m2].
-      exact (tr (coeq (m2, m1))).
+      exact (grp_compl m2 m1).
     - intros [[a b] s].
       simpl.
       apply (ap tr).
@@ -192,29 +194,98 @@ Section Group_completion.
     Build_Abelian_Group
       (Build_Group
          (Build_Monoid
-            (grp_compl_set S) 
-            (grp_compl_mult) grp_compl_id grp_compl_assoc grp_compl_lid grp_compl_rid )
+            grp_compl_set
+            grp_compl_mult grp_compl_id grp_compl_assoc grp_compl_lid grp_compl_rid )
          grp_compl_inv grp_compl_linv grp_compl_rinv)
       grp_compl_sym.
 
   
   (*Defining the (monoid) homomorphism from a monoid to its group completion*)
-   Definition S_to_grpcmplS : Homomorphism S group_completion.
-     srapply @Build_Homomorphism.
+  Definition to_groupcompletion : Homomorphism S group_completion.
+    srapply @Build_Homomorphism.
     - (*The map on set level*)
       intro m.
-      exact (tr (coeq (m, mon_id S))).
+      exact (grp_compl m (mon_id S)).
     - (*The map preserves identity*)
       exact idpath.
     - (*The map is an isomorphism*)
       intros m1 m2.
       apply (ap tr); apply (ap coeq).
       apply path_prod.
-        exact idpath.
-        exact (mon_lid (mon_id S))^.
+      exact idpath.
+      exact (mon_lid (mon_id S))^.
   Defined.
 
+  (* Definition eta_group_completion : forall g : group_completion, *)
+  (*   exists ab : S*S, g = grp_compl (fst ab) (snd ab). *)
+  (* Proof. *)
+  (*   srapply @Trunc_ind. intro aa.  *)
+
+  Definition gcp : forall a b : S,
+                     grp_compl a b = to_groupcompletion a - to_groupcompletion b.
+  Proof.
+    intros a b.
+    unfold to_groupcompletion. simpl. unfold grp_compl. apply (ap tr).
+    refine ((cp (a, b, mon_id S))^ @ _). apply (ap coeq). unfold as_bs.
+    apply (ap (fun c : S => (a + mon_id S, c))).
+    exact (mon_rid b @ (mon_lid b)^).
+  Defined.
+
+  Definition badname : forall a b : S,
+                         to_groupcompletion a = to_groupcompletion b -> exists s : S, a + s = b + s.
+  Proof.
+    intros a b p.
+    assert (grp_compl a b = grp_id (group_completion)).
+    { refine (gcp a b @ _).
+      apply (grp_moveR_gV (G := group_completion)).
+      exact (p @ (grp_lid (to_groupcompletion b))^). }
+    unfold group_completion in X. simpl in X. unfold grp_compl_id in X. unfold grp_compl in X.
+  Admitted.
+
+
+     (* Check (equiv_path_Tr (n := -1) (coeq (a, b)) (coeq (mon_id S, mon_id S)))^-1 X. *)
+  (*   (* `Arguments foo ... : simpl nomatch` *) *)
+  
+
+  Definition injective_to_groupcompletion (right_cancel : forall a b s : S, a + s = b + s -> a = b) :
+    forall a b : S, to_groupcompletion a = to_groupcompletion b -> a = b.
+  Proof.
+    (* assert (retract : group_completion -> S). *)
+    (* { srapply @Trunc_rec. exact (mon_isset S). *)
+    (*   srapply @Coeq_rec. *)
+    (*   - exact fst. *)
+    (*   - intros [[a b] s]. simpl. *)
+    (*     refine (mon_assoc^ @ _). *)
+    (*     refine (grp_whiskerL  *)
+    (*     apply (ap (fun c => a + c)). *)
+    (*     refine (mon_assoc @ _). *)
+        
+    
+    intros a b.
+    unfold to_groupcompletion. simpl. unfold grp_compl.
+    intro p.
+    (* Check grp_moveR_gV (p @ (grp_lid (to_groupcompletion b))^). *)
+    (* apply grp_moveL_1M. *)
+    assert (to_groupcompletion a - to_groupcompletion b = grp_id _).
+    { apply grp_moveR_gV.
+      refine (p @ _).
+      exact (grp_lid (to_groupcompletion b))^. }
+    unfold to_groupcompletion in p. simpl in p.
+
+    (* apply (grp_whiskerR (c := grp_id _)) in p. *)
+    (* apply grp_moveL_Vg in p. *)
+    (* unfold grp_inv in p.  *)
+    (* unfold to_groupcompletion in p. simpl in p. *)
+    (* set (p' := ((equiv_path_Tr (coeq (a , mon_id S)) (coeq (b, mon_id S)))^-1 p)). (*This takes long *) time to run. . .*)
+    Admitted.
+    
+
+  
+    
 End Group_completion.
+
+Arguments to_groupcompletion {S}.
+(* Arguments grp_compl {S}. *)
 
 Section Functoriality.
   Open Scope monoid_scope.
@@ -228,11 +299,11 @@ Section Functoriality.
       srapply @Coeq_rec.
       + (* (a,b) => (f a, f b) *)
         intros [m n].
-        exact (tr (coeq (hom_map f m, hom_map f n))).
+        exact (tr (coeq (f m, f n))).
       + (*The map is well defined.*)
         intros [[a b] s].
         apply (ap tr).
-        refine (_ @ cp (hom_map f a, hom_map f b, hom_map f s)).
+        refine (_ @ cp (f a, f b, f s)).
         apply (ap coeq).
         apply path_prod; apply preserve_mult.
     - (*Preserves identity*)
@@ -255,8 +326,8 @@ Section Functoriality.
         intro abs. apply (istrunc_truncation 0).
   Defined.        
         
-  Definition natural_S_to_grpcmplS {S S' : Symmetric_Monoid} (h : Hom S' S):
-    compose_hom (S_to_grpcmplS S) h = compose_hom (functor_group_completion h) (S_to_grpcmplS S').
+  Definition natural_to_groupcompletion {S S' : Symmetric_Monoid} (h : Hom S' S):
+    to_groupcompletion oH h = (functor_group_completion h) oH to_groupcompletion.
   Proof.
     apply path_hom.
     apply path_forall.
@@ -273,15 +344,15 @@ Section Adjointness.
   (*Prove that group completion is left adjoint to the forgetful functor from abelian groups to symmetric monoids*)
   Open Scope monoid_scope.
   
-  Definition hom_map_extend_to_inv {S : Symmetric_Monoid} {A : Abelian_Group} :
-    Homomorphism S A -> ((group_completion S) -> A).
+  Definition hom_map_extend_to_inv {S : Symmetric_Monoid} {G : Group} :
+    Homomorphism S G -> ((group_completion S) -> G).
   Proof.
     intro f.
     refine (Trunc_rec (H:=grp_isset _) _).
     srapply @Coeq_rec.
     + (*The variable is fixed*)
       intro m12.
-      exact ((hom_map f (fst m12)) - (hom_map f (snd m12))).
+      exact ((f (fst m12)) - (f (snd m12))).
     + (*The variable runs along cp (i.e. map is well defined)*)
       intros [[a b] s]. simpl.
       refine (path_group2 (preserve_mult f) (ap grp_inv (preserve_mult f)) @ _).
@@ -293,8 +364,8 @@ Section Adjointness.
       exact (grp_lid _)^ .
   Defined.
 
-  Definition extend_to_inv {S : Symmetric_Monoid} {A : Abelian_Group} :
-    Homomorphism S A -> Homomorphism (group_completion S) A.
+  Definition extend_to_inv {S : Symmetric_Monoid} {G : Group} :
+    Homomorphism S G -> Homomorphism (group_completion S) G.
   Proof.
     intro f.
     refine (Build_Homomorphism _ _ (hom_map_extend_to_inv f) (grp_rinv _) _).
@@ -321,23 +392,29 @@ Section Adjointness.
       srapply @Coeq_ind.
       + (*First variable fixed*)
         intros [m1 m2]. simpl.
-        refine (grp_whiskerR (preserve_mult f) @ _).
+        refine (grp_whiskerL (ap grp_inv (ap f (mon_sym))) @ _).
+        refine (path_group2 (preserve_mult f) ((ap grp_inv (preserve_mult f))) @ _).
         refine (grp_assoc^ @ _ @ grp_assoc).
         apply grp_whiskerL.
-        refine (grp_whiskerL (ap grp_inv (preserve_mult f)) @ _).
         refine (grp_whiskerL grp_inv_distr @ _).
-        refine (_ @ grp_sym).
-        exact grp_assoc.
+        refine (grp_assoc @ _ @ grp_assoc^).
+        apply grp_whiskerR.
+        apply grp_moveR_gV.
+        refine (_ @ grp_assoc).
+        apply grp_moveL_Vg.
+        refine ((preserve_mult f)^ @ _ @ preserve_mult f).
+        apply (ap f). apply mon_sym.
       + (*First variable runs along cp*)
-        intro abs. apply (grp_isset A).
+        intro abs. apply (grp_isset G).
     - (*Second variable runs along cp*)
-      intro abs. apply (grp_isset A).
+      intro abs. apply (grp_isset G).
   Defined.
 
-  Theorem grp_compl_adjoint (S : Symmetric_Monoid) (A: Abelian_Group) :
-    Homomorphism S A <~> Homomorphism (group_completion S) A.
+  
+  Theorem grp_compl_adjoint (S : Symmetric_Monoid) (G : Group) :
+    Homomorphism S G <~> Homomorphism (group_completion S) G.
   Proof.
-    refine (equiv_adjointify extend_to_inv (fun f => compose_hom f (S_to_grpcmplS S)) _ _).
+    refine (equiv_adjointify extend_to_inv (fun f => f oH to_groupcompletion) _ _).
     (*Prove that the maps are inverses*)
     - intro f.
       refine (path_hom _ _ _) ; simpl.
@@ -345,7 +422,7 @@ Section Adjointness.
       (*Trunc_ind uses about 5 minutes to find a proof of this, faster to prove it manually.*)
       assert (Pt : forall m : (group_completion S),
                    IsTrunc 0
-                           (hom_map_extend_to_inv (compose_hom f (S_to_grpcmplS S)) m = hom_map f m)).
+                           (hom_map_extend_to_inv (f oH to_groupcompletion) m = f m)).
       { intro m. exact (@trunc_succ -1 _ (mon_isset _ _ _)). }
       refine (@Trunc_ind 0 (Coeq (as_bs S) fst) _ Pt _ ).
       srapply @Coeq_ind. 
@@ -354,14 +431,14 @@ Section Adjointness.
         simpl.
         apply grp_moveR_gV.
         refine (_ @ preserve_mult f).
-        apply (ap (hom_map f)); apply (ap tr).
+        apply (ap f); apply (ap tr).
         refine ((cp (m1, mon_id S, m2))^ @ _); unfold as_bs.
         apply (ap coeq).
         apply (ap (fun s : S => (m1 + m2, s))).
         exact (mon_lid m2 @ (mon_rid m2)^).
       + (*Variable runs along cp*)
         intro abs.
-        apply (grp_isset A).
+        apply (grp_isset G).
     - intro f.
       refine (path_hom _ _ _) ; simpl.
       apply path_forall.
@@ -371,6 +448,12 @@ Section Adjointness.
       exact (grp_whiskerL (preserve_id f)^ ).
   Defined.
 
-  (*Naturality follows from [natural_S_to_grpcmplS], since the map [Hom (group_completion S) A -> Hom S A is exactly precomposing with [S_to_grpcmplS].*)
+  (*Naturality follows from [natural_to_groupcompletion], since the map [Hom (group_completion S) A -> Hom S A is exactly precomposing with [to_groupcompletion].*)
+
+  Definition grp_compl_unit (S : Symmetric_Monoid) : Hom S (group_completion S) :=
+    (grp_compl_adjoint S (group_completion S))^-1 idhom.
+  Definition grp_compl_counit (A : Abelian_Group) : Hom (group_completion A) A :=
+    grp_compl_adjoint A A idhom.
+  
 
 End Adjointness.
